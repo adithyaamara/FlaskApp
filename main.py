2c6cd6de2b1912a440395836217e0c7d568eb394
Adithya Amara <adithya.amara@gmail.com>
2021-09-26 12:50:49 +0530

    added icon,registartion page for devs working,login mechanism for admins

--- main.py
+++ main.py
@@ -2,37 +2,24 @@ from flask import Flask,render_template,jsonify,url_for,redirect,request
 from flask_mysqldb import MySQL
 import json
 import time
-'''
 
-import MySQLdb.cursors
-import re
-
-
-
-'''
 #Read MySql Config from json
 config = json.load(open("config.json",'r'))
+#print(config)
+
 #Craete an object of Flak class
 app = Flask(__name__)
 
-#print(config)
 #Initiate MySql Connection from flask app
-
 app.config['MYSQL_HOST'] = config['MYSQL_HOST']
 app.config['MYSQL_USER'] = config['MYSQL_USER']
 app.config['MYSQL_PASSWORD'] = config['MYSQL_PASSWORD']
 app.config['MYSQL_DB'] = config['MYSQL_DB']
-#print(config)
 mysql = MySQL(app)
-#Creating a connection cursor
-#cursor = mysql.connection.cursor()
-#Executing SQL Statements
-#cursor.execute('select * from test')
-#data = cursor.fetchone()
-#print(data)
-#Closing the cursor
-#cursor.close()
-#Routers
+
+#Routes
+
+#------Jinja Templating Example-------------#
 @app.route('/result')
 def display():
     """Result Grabber with submit and reset"""
@@ -50,11 +37,15 @@ def marks():
     if request.method == 'GET':
         return "Get is blocked"
     return render_template('marks.html', result = dict)
+#-----------------------------------------------------------#
 
-@app.route('/')
+@app.route('/',methods = ['GET'])
 def admin():
     """Default route, redirects to API HOME"""
-    return redirect(url_for('default'))
+    return redirect(url_for('home'))
+@app.route('/api',methods = ['GET'])
+def home():
+    return "<h2 align=\"center\">Flask App V1.0 - Home Page to be built</h2><br><a href='/api/help'>Click Here for Help</a>"
 
 @app.route("/dev_register",methods = ['GET'])
 def dev_register():
@@ -62,7 +53,6 @@ def dev_register():
 
 @app.route('/devregisterdb',methods=['POST'])
 def devregisterdb():
-    
     if request.method == 'POST':
         newdev = {}
         newdev["dev_name"] = request.form["name"]
@@ -73,38 +63,43 @@ def devregisterdb():
         #print(newdev)
         try:
             cursor = mysql.connection.cursor()
-            query = "INSERT INTO developers values(\""+ newdev["dev_name"] + "\",\"" + newdev["dev_email"] + "\"," + str(newdev["dev_phone"]) + ",\"" + newdev["dev_city"] + "\"," +str(newdev["dev_age"])+ ")"
-            print(query)
+            query = "select * from developers WHERE PHONE=\""+str(newdev["dev_phone"])+"\" LIMIT 1"
+            #print(query)
             cursor.execute(query)
-            cursor.commit()
-            cursor.close()
-            return render_template('reg_status.html',status="Successful",details=newdev)
-        
+            if cursor.fetchone() is not None:
+                cursor.close()
+                return render_template('reg_status.html',status="duplicate")
+            else:
+                query = "INSERT INTO developers values(\""+ str(newdev["dev_name"]) + "\",\"" + str(newdev["dev_email"]) + "\",\"" + str(newdev["dev_phone"]) + "\",\"" + str(newdev["dev_city"]) + "\"," + (newdev["dev_age"])+ ")"
+                #print(query)
+                cursor.execute(query)
+                time.sleep(2)
+                mysql.connection.commit()
+                cursor.close()
+                return render_template('reg_status.html',status="Successful",details=newdev)
         except Exception as e:
             print("/deveregisterdb -> ",e)
             return render_template('reg_status.html',status="Fail")
-            
-@app.route('/api')
-def default():
-    return "<h2 align=\"center\">Flask App V1.0</h2><br><a href='/api/help'>Click Here for Help</a>"
-
-@app.route('/validate', methods = ['POST','GET'])
-def validate():
-    """Validates Login Received from Login Screen"""
-    if request.method == 'POST':
-        user = request.form['nm']
-        ph = request.form['ph']
-        return redirect(url_for('success',name=user,phone=ph))
-    if request.method == 'GET':
-        user = request.args.get('nm')
-        return redirect(url_for('fail'))
-    else:
-        return redirect(url_for('fail'))
-
+#Admin Login and validation#
 @app.route('/login')
 def login():
     """Login Screen"""
     return render_template('login.html')
+
+@app.route('/validate', methods = ['POST'])
+def validate():
+    """Validates Login Received from Login Screen"""
+    if request.method == 'POST':
+        id = request.form['id']
+        pwd = request.form['pwd']
+        query = "SELECT * FROM admins WHERE phone=\"" + str(id) + "\" and pwd=\"" + str(pwd) + "\" LIMIT 1"
+        cursor = mysql.connection.cursor()
+        cursor.execute(query)
+        if cursor.fetchone() is not None:   
+            cursor.close()
+            return f"Login Succesful [Need to redirect to admin dashboard]! Welcome Admin ID- {id}"
+        else:
+            return "<h2>Unauthorized Access!! <a href='/login'>Try Aagain</a></h2>"
 
 @app.route('/api/help', methods = ['GET'])
 def help():
@@ -115,10 +110,12 @@ def help():
             func_list[rule.rule] = app.view_functions[rule.endpoint].__doc__
     return render_template('site-map.html', result = func_list)
 
-@app.route('/charts',methods=['GET'])
+@app.route('/charts',methods=['POST','GET'])
 def charts():
     """Demo of google charts on static data"""
-    return render_template('gcharts.html')
-
+    if request.method == 'POST':
+        return render_template('gcharts.html')
+    else:
+        return redirect(url_for('login'))
 if __name__ == '__main__':
     app.run('127.0.0.1',4444,True)
